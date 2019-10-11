c9052a218cb67c6b71edcaa889fcaa9c369d6e6c
chaparrovision <david.chaparro@ymail.com>
2019-10-11 13:27:43 -0400

    Changed Home page to a functional component

---
+++ src/components/paypal.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export const PayPalComponent: React.FC = () => {
+
+    return(
+        <>
+            <h1>PayPal</h1>
+        </>
+    )
+}
